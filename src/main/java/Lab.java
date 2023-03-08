7a83ae78c839cecc906c7a4771502ef4813c4415
Jason Torres <jasontorres585@icloud.com>
2023-03-08 03:15:57 +0000

    for loop lab

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -41,9 +41,10 @@ public class Lab {
      */
     public String returnNumbers(int start, int end){
         String buildingString = "";
-
-//        code here
-
-        return buildingString;
+        for(int i = start; i < end; i++){
+        buildingString = buildingString + i + " ";
+        System.out.print(i);
+       }
+         return buildingString;
     }
 }
